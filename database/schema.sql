8334dfbea6b7d6fc442b53589eed1a0848f1d02d
chazmcgrill <charlietaylordev@gmail.com>
2018-04-22 19:24:26 +0100

    Added DB insertions and Created local DB

--- database/schema.sql
+++ database/schema.sql
@@ -1,9 +1,18 @@
 CREATE DATABASE top_ten;
-USE DATABASE top_ten;
+USE top_ten;
 
 CREATE TABLE movies (
   id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
   imdb_id VARCHAR(50) NOT NULL,
+  img_url VARCHAR(255) NOT NULL,
   ranking INT
-)
+);
+
+INSERT INTO movies (title, imdb_id, img_url, ranking) 
+VALUES (
+  "Pulp Fiction", 
+  "tt0110912", 
+  "https://ia.media-imdb.com/images/M/MV5BMTkxMTA5OTAzMl5BMl5BanBnXkFtZTgwNjA5MDc3NjE@",
+  1
+);
